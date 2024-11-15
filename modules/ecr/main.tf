1ead3b35f0efdaa4f887e3888b1857430fc2760b
neko-kick <n26kdgrat@gmail.com>
2024-11-16 06:46:25 +0900

    Add ECR Service

--- modules/ecr/main.tf
+++ modules/ecr/main.tf
@@ -1,13 +1,13 @@
 # ECR
 locals {
-  repository_name = format("%s-%s-ecr-%s-%02d", var.pj_tags.name, var.pj_tags.env, var.ecr.prefix, 1)
+  repository_name       = format("%s-%s-ecr-%s-%02d", var.pj_tags.name, var.pj_tags.env, var.ecr.prefix, 1)
 }
 
-resource "aws_ecr_repository" "main" {
+resource "aws_ecr_repository" "repo" {
   name                 = local.repository_name
-  image_tag_mutability = var.ecr.is_enable_immutable ? IMMUTABLE : MUTABLE
+  image_tag_mutability = var.ecr.is_enable_immutable ? "IMMUTABLE" : "MUTABLE"
   encryption_configuration {
-    encryption_type = AES256
+    encryption_type = "AES256"
   }
   image_scanning_configuration {
     scan_on_push = true
